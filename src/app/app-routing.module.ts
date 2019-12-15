143803fe238b700dfca0d6994ca64310b1d0fca7
rockstar777 <puneetbindal8989@gmail.com>
2019-12-15 22:40:11 +0530

    home page completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+//components
+import {HomeComponent} from './pages/home/home.component';
+import { DetailComponent } from './pages/detail/detail.component';
+import { UserlistComponent } from './pages/userlist/userlist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'mylist', component: UserlistComponent},
+  { path: 'city/:id', component: DetailComponent},
+  { path: '**', redirectTo: 'home', pathMatch: 'full' }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DetailComponent } from './pages/detail/detail.component';
+import { CardComponent } from './utils/card/card.component';
+import { SearchComponent } from './utils/search/search.component';
+import { UserlistComponent } from './pages/userlist/userlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DetailComponent,
+    CardComponent,
+    SearchComponent,
+    UserlistComponent
   ],
   imports: [
     BrowserModule,
